c4706bd5e6067b684dc3ea87cc261448b02875de
Ryan Park <ryan.developer99@gmail.com>
2021-10-11 11:22:14 +0900

    add: heroku-postbuild

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,9 +15,13 @@ app.use("/api/auth", userRouter);
 const PORT = process.env.PORT || 8000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/build")));
+  // app.use(express.static(path.join(__dirname, "../../client/build")));
+  app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../../client/build/index.html"));
+    res.sendFile(
+      path.resolve(__dirname + "../../client", "build", "index.html")
+    );
   });
 }
 
