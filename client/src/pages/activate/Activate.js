db133d19fdf812387b5bb34b44941b0c59080fa7
Ryan Park <ryan.developer99@gmail.com>
2021-10-11 20:26:21 +0900

    fix: activate account

--- client/src/pages/activate/Activate.js
+++ client/src/pages/activate/Activate.js
@@ -7,60 +7,59 @@ import Layout from "../../containers/layout/Layout";
 import { Redirect } from "react-router";
 
 const Activate = ({ match }) => {
-  // const [values, setValues] = useState({
-  //   username: "",
-  //   token: "",
-  //   show: true,
-  // });
+  const [values, setValues] = useState({
+    username: "",
+    token: "",
+    show: true,
+  });
 
   const [isClicked, setIsClicked] = useState(false);
 
-  // useEffect(() => {
-  //   let token = match.params.token;
-  //   console.log(token);
-  //   let { username } = jwt.decode(token);
-  //   console.log(username);
-  //   if (token) {
-  //     setValues({ ...values, username, token });
-  //   }
-  // }, []);
+  useEffect(() => {
+    let token = match.params.token;
+    console.log(token);
+    let { username } = jwt.decode(token);
+    console.log(username);
+    if (token) {
+      setValues({ ...values, username, token });
+    }
+  }, []);
 
-  // const { username, token, show } = values;
+  const { username, token, show } = values;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("clicked");
 
-    // try {
-    //   const res = await axios.post("/api/auth/account-activation", {
-    //     token,
-    //   });
-    //   console.log("Sign Up Success");
-    //   console.log(res);
-    //   toast.success(res.data.message);
-    //   toast.success("You will be moved to Login page in 3 seconds");
-    //   setTimeout(() => {
-    //     setIsClicked(true);
-    //   }, 3000);
-    // } catch (err) {
-    //   console.log("Sign Up False");
-    //   console.log(err);
-    //   console.log("err response: ", err.response);
-    //   toast.error(err.response.data.err);
-    // }
+    try {
+      const res = await axios.post("/api/auth/account-activation", {
+        token,
+      });
+      console.log("Sign Up Success");
+      console.log(res);
+      toast.success(res.data.message);
+      toast.success("You will be moved to Login page in 3 seconds");
+      setTimeout(() => {
+        setIsClicked(true);
+      }, 3000);
+    } catch (err) {
+      console.log("Sign Up False");
+      console.log(err);
+      console.log("err response: ", err.response);
+      toast.error(err.response.data.err);
+    }
   };
 
   const activationLink = () => (
     <div>
-      {/* <h1>Hello {username}, Are you ready to activate your account?</h1> */}
-      <h1>Hello Are you ready to activate your account?</h1>
+      <h1>Hello {username}, Are you ready to activate your account?</h1>
       <button onClick={handleSubmit}>Activate Account</button>
     </div>
   );
 
   return (
     <Layout>
-      {/* {isClicked ? <Redirect to="/signin2" /> : null} */}
+      {isClicked ? <Redirect to="/signin2" /> : null}
       {activationLink()}
       <ToastContainer />
     </Layout>
